8044264f2668b2e6570c7901b5639006bd68282b
singharaj usai <djjd22938@gmail.com>
2023-08-04 21:28:20 -0400

    vehicle models save

--- src/components/pages/VehicleModels/CarDetails.jsx
+++ src/components/pages/VehicleModels/CarDetails.jsx
@@ -1,30 +1,59 @@
 import React from "react";
 
 const CarDetails = ({ car }) => {
-    const { year, color, transmission } = car.description;
+    const { brand, model, rent, year, color, doors, transmission, fuel } = car.description;
 
     return (
         <div>
             <h2 className="text-2xl font-bold mb-4">Car Details</h2>
             <table className="w-full table-auto border-collapse">
                 <tbody>
+                <tr className="w-full bg-red-500">
+                    <td className="px-4 py-3 text-white font-semibold text-xl" colSpan="2">
+                        {rent} / <span className="text-md">per day</span>
+                    </td>
+                </tr>
                 <tr className="border-b">
-                    <td className="px-4 py-3 font-semibold text-gray-800 bg-gray-100">
+                    <td className="px-4 py-3 font-semibold text-gray-800 bg-gray-100 w-1/3">
+                        Brand:
+                    </td>
+                    <td className="px-4 py-3">{brand}</td>
+                </tr>
+                <tr className="border-b">
+                    <td className="px-4 py-3 font-semibold text-gray-800 bg-gray-200 w-1/3">
+                        Model:
+                    </td>
+                    <td className="px-4 py-3">{model}</td>
+                </tr>
+                <tr className="border-b">
+                    <td className="px-4 py-3 font-semibold text-gray-800 bg-gray-100 w-1/3">
                         Year:
                     </td>
                     <td className="px-4 py-3">{year}</td>
                 </tr>
                 <tr className="border-b">
-                    <td className="px-4 py-3 font-semibold text-gray-800 bg-gray-200">
+                    <td className="px-4 py-3 font-semibold text-gray-800 bg-gray-200 w-1/3">
                         Color:
                     </td>
                     <td className="px-4 py-3">{color}</td>
                 </tr>
                 <tr>
-                    <td className="px-4 py-3 font-semibold text-gray-800 bg-gray-100">
+                    <td className="px-4 py-3 font-semibold text-gray-800 bg-gray-100 w-1/3">
+                        Doors:
+                    </td>
+                    <td className="px-4 py-3">{doors}</td>
+                </tr>
+                <tr className="border-b">
+                    <td className="px-4 py-3 font-semibold text-gray-800 bg-gray-200 w-1/3">
                         Transmission:
                     </td>
                     <td className="px-4 py-3">{transmission}</td>
+                </tr>
+                <tr>
+                    <td className="px-4 py-3 font-semibold text-gray-800 bg-gray-100 w-1/3">
+                        Fuel:
+                    </td>
+                    <td className="px-4 py-3">{fuel}</td>
                 </tr>
                 {/* Add more rows for other details */}
                 </tbody>
--- src/components/pages/VehicleModels/VehicleModels.jsx
+++ src/components/pages/VehicleModels/VehicleModels.jsx
@@ -10,10 +10,14 @@ const vehicleModelsData = [
     name: "Audi A1 S-Line",
     image: AudiS1,
     description: {
+      rent: "$45",
+      brand: "Audi",
+      model: "A1 S-Line",
       year: "2012",
       color: "White",
+      doors: "4 Door",
       transmission: "Manual",
-      // Add more details as needed
+      fuel: "Gasoline",
     },
   },
   {
@@ -21,13 +25,16 @@ const vehicleModelsData = [
     name: "Dodge Challenger",
     image: Challenger,
     description: {
+      rent: "$85",
+      brand: "Dodge",
+      model: "Challenger",
       year: "2020",
       color: "Red",
+      doors: "2 Door",
       transmission: "Automatic",
-      // Add more details as needed
+      fuel: "Gasoline",
     },
   },
-  // Add more car models
 ];
 
 const VehicleModels = () => {
@@ -38,15 +45,15 @@ const VehicleModels = () => {
   };
 
   return (
-      <section className="py-8">
+      <section className="px-4 py-8">
         <div className="container mx-auto grid md:grid-cols-3 gap-8">
           <div className="md:col-span-1">
-            <div className="flex flex-col space-y-4">
+            <div className="flex flex-col space-y-2">
               {vehicleModelsData.map((car) => (
                   <button
                       key={car.id}
-                      className={`p-4 border border-gray-300 rounded-md ${
-                          selectedCar.id === car.id ? "bg-blue-500 text-white" : ""
+                      className={`p-4 border border-gray-300 font-bold rounded ${
+                          selectedCar.id === car.id ? "hover:text-red-500 hover:bg-gray-50 transition ease-out duration-200 text-md text-gray-50 font-bold bg-red-500 border-red-500 border-2" : ""
                       }`}
                       onClick={() => handleCarSelect(car)}
                   >
